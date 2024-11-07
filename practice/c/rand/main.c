a39bde59e076246ee5cdd98f158b8fe0902846cc
scchang <a0928136004@gmail.com>
2024-11-07 21:16:09 +0800

    modified

--- practice/c/rand/main.c
+++ practice/c/rand/main.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<time.h>
-#include<string.h>
 
 int main(int argc, char *argv[])
 {
-    int i, j, cnt[6];
+    int i, dice[6]={0};
     srand((unsigned)time(NULL));
-    memset(cnt, 0, sizeof(cnt));
+
     for(i=0; i<50000; i++){
-        for(j=0; j<6; j++){
-            if((rand()%6) == j)
-                cnt[j]++;
-        }
+        dice[rand()%6]++;
     }
-    for(j=0; j<6; j++){
-        printf("Times of %d = %d\n", j+1, cnt[j]);
-        printf("Probability = %f\n", (float)cnt[j]/50000);
+    for(i=0; i<6; i++){
+        printf("#%d: %d times, probability = %f\n", i+1, dice[i], dice[i]/50000.0);
     }
     system("PAUSE");
     return 0;
