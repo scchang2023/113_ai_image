96eb0366672aeb483e948972e1971d3f72eb54ad
scchang <a0928136004@gmail.com>
2024-11-07 19:34:27 +0800

    modified

--- practice/c/for1to100/main.c
+++ practice/c/for1to100/main.c
@@ -2,9 +2,9 @@
 
 int main(int argc, char *argv[])
 {
-    int i, sum=0;
+    int sum=0;
 
-    for(i=0; i<100; i++)
+    for(int i=1; i<=100; i++)
         sum+=i;
 
     printf("1+2+3+...+100=%d\n", sum);
